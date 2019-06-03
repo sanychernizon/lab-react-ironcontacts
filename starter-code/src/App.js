bf5badb8dd277b74e781ae1099ffd36fea1672c1
Sany Chernizon <chernizon@gmail.com>
2019-06-03 20:56:59 -0300

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>IronContacts</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      {
+        contacts.map((item, idx) => {
+          if(idx < 5){
+            return <tr>
+              <td><img src={item.pictureUrl}/></td>
+              <td>{item.name}</td>
+              <td>{item.popularity.toFixed(2)}</td>
+              </tr>
+          }
+        })
+      }
+      </table>
       </div>
     );
   }
